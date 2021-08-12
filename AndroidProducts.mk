2b30746af344eda383ee7d7a752af6ff3278f768
qingkong9579 <45691149+qingkong9579@users.noreply.github.com>
2021-08-12 08:10:14 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_violet.mk
+    $(LOCAL_DIR)/exthm_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_violet-userdebug \
-    aosp_violet-user \
-    aosp_violet-eng
+    exthm_violet-userdebug \
+    exthm_violet-user \
+    exthm_violet-eng
